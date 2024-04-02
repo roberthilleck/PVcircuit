1f18d6b83a6f77dbd51aef5c941da08a6e5d2708
rob-kestrel <robert.witteck@nrel.gov>
2024-04-02 11:20:32 -0600

    adding NSRD support

--- pvcircuit/__init__.py
+++ pvcircuit/__init__.py
@@ -31,14 +31,8 @@ import pvcircuit.junction as junction
 import pvcircuit.multi2T as multi2T
 import pvcircuit.qe as qe
 import pvcircuit.tandem3T as tandem3T
-
 from pvcircuit.submodules.NRELMeteorological import environmental
 
-
-# expose constructors to package's top level
-Junction = junction.Junction
-TK = junction.TK
-Vth = junction.Vth
 Jdb = junction.Jdb
 
 Multi2T = multi2T.Multi2T
@@ -59,9 +53,21 @@ Meteo = EY.Meteo
 
 sync = environmental.sync
 
+environmental = environmental
+
 Meteorological = environmental.Meteorological
 Spectra = environmental.Spectra
 
+
+# Expose nsrdb
+try:
+    from pvcircuit.submodules.NRELMeteorological import config, nsrdb
+
+    NSRDB = nsrdb.NSRDB
+    config = config
+
+except ModuleNotFoundError:
+    print("NSRDB is not avaialbe")
 
 
 #
