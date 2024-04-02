fa2f267d8220454855fa97da2bfb472d0645b83c
Robert Witteck <robert.witteck@nrel.gov>
2024-04-02 10:52:40 -0600

    add tests for junction class

--- tests/test_junction.py
+++ tests/test_junction.py
@@ -3,6 +3,7 @@ import os
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+import pytest
 from pvlib import ivtools, pvsystem
 
 import pvcircuit as pvc
@@ -10,10 +11,10 @@ from pvcircuit import Junction, Multi2T
 
 if __name__ == "__main__":
 
-    root = os.path.dirname(os.path.abspath(__file__))
+    root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-    fp = os.path.join(root,"data","Pvsk_1.70MA-free_JV.csv")
-#     fp = os.path.join(root,"IBC2x2.csv")
+    fp = os.path.join(root, "data", "Pvsk_1.70MA-free_JV.csv")
+    #     fp = os.path.join(root,"IBC2x2.csv")
 
     A = 0.122
     TC = 25  # [degC]
@@ -21,9 +22,9 @@ if __name__ == "__main__":
 
     data = pd.read_csv(fp)
     # Measured terminal voltage.
-    voltage = data['v'].to_numpy(np.double) #[V]
+    voltage = data["v"].to_numpy(np.double)  # [V]
     # Measured terminal current.
-    current = data['i'].to_numpy(np.double) / 1000 * A #[A]
+    current = data["i"].to_numpy(np.double) / 1000 * A  # [A]
 
     sort_id = np.argsort(voltage)
 
@@ -31,10 +32,10 @@ if __name__ == "__main__":
     current = current[sort_id]
 
     photocurrent, saturation_current, resistance_series, resistance_shunt, nNsVth = ivtools.sde.fit_sandia_simple(voltage, current)
-    d_fitres = pvsystem.singlediode(photocurrent, saturation_current, resistance_series, resistance_shunt, nNsVth, ivcurve_pnts=100, method='brentq')
+    d_fitres = pvsystem.singlediode(photocurrent, saturation_current, resistance_series, resistance_shunt, nNsVth, ivcurve_pnts=100, method="brentq")
 
-    fit_voltage = d_fitres['v']
-    fit_current = d_fitres['i']
+    fit_voltage = d_fitres["v"]
+    fit_current = d_fitres["i"]
 
     Jext = photocurrent / A  # [A/cm^2]
     n = nNsVth / pvc.junction.Vth(TC)
@@ -50,7 +51,6 @@ if __name__ == "__main__":
     PVK = Multi2T(name="Psk", area=A, Jext=Jext, Eg_list=[Eg], n=[n], J0ratio=[J0ratio])
     PVK.set(Rs2T=Rser, Gsh=Gsh)
     PVK.j[0]
-
 
     MPP = PVK.MPP()
 
@@ -71,14 +71,146 @@ if __name__ == "__main__":
 
     Vboth = np.concatenate((pvc_voltage_calc, pvc_voltage_set), axis=None)
     Iboth = np.concatenate((pvc_current_set, pvc_current_calc), axis=None)
-            # sort
+    # sort
     p = np.argsort(Vboth)
     Vlight = Vboth[p]
     Ilight = -1 * Iboth[p]
 
-
-    fig,ax = plt.subplots()
-    ax.plot(voltage,current, ".")
-    ax.plot(Vlight,Ilight)
+    fig, ax = plt.subplots()
+    ax.plot(voltage, current, ".")
+    ax.plot(Vlight, Ilight)
     # ax.plot(Vl,Il, "--")
     plt.show()
+
+    print(pvc.junction.Vth(25))
+
+
+# %%
+def test_basic_functions():
+
+    # Test thermal voltage
+    np.testing.assert_almost_equal(pvc.junction.Vth(25), 0.02569257912108585)
+
+    # Test temperature conversion
+    np.testing.assert_almost_equal(pvc.junction.TK(25), 298.15)
+
+    # Test detailed balance current
+    Eg = 1.12
+    TC = 25
+    EgkT = Eg / pvc.junction.Vth(TC)
+    np.testing.assert_almost_equal(pvc.junction.Jdb(TC=TC, Eg=Eg, sigma=0), 6.249646867228706e-17)
+
+    # Comapre to old PV with sigma = 0
+    np.testing.assert_almost_equal(pvc.junction.Jdb(TC=TC, Eg=Eg, sigma=0), pvc.junction.DB_PREFIX * pvc.junction.TK(TC) ** 3.0 * (EgkT * EgkT + 2.0 * EgkT + 2.0) * np.exp(-EgkT))
+
+
+# %%
+@pytest.fixture
+def junction():
+    return Junction()
+
+
+def test_junction_str(junction):
+
+    with open(os.path.join("tests", "Junction.txt"), "r", encoding="utf8") as fid:
+        junction_in = [line.rstrip().strip() for line in fid]
+
+    for i, r in enumerate(junction_in):
+        junction_out = junction.__str__().split("\n")[i].strip()
+        assert junction_out in junction_in
+
+
+def test_junction_setter(junction):
+
+    junction.set(n=[1, 2])
+    np.testing.assert_array_equal(junction.n, np.array([1, 2]))
+
+    junction.set(area=1.23)
+    np.testing.assert_almost_equal(junction.lightarea, 1.23)
+    np.testing.assert_almost_equal(junction.totalarea, 1.23)
+
+    with pytest.raises(ValueError, match=r"invalid class attribute test"):
+        junction.set(test=-1)
+    with pytest.raises(ValueError, match=r"invalid class attribute avalanche"):
+        junction.set(avalanche=1)
+    with pytest.raises(ValueError, match=r"invalid class attribute mrb"):
+        junction.set(mrb=1)
+
+    junction.set(RBB="bishop")
+    junction.set(avalanche=1)
+
+    with pytest.raises(ValueError, match=r"invalid class attribute J0rb"):
+        junction.set(J0rb=1)
+
+    junction.set(RBB="JFG")
+    junction.set(J0rb=1)
+
+
+def test_junction_properties(junction):
+
+    np.testing.assert_almost_equal(junction.Jphoto, 0.04)
+    np.testing.assert_allclose(junction.J0, [1.3141250302231388e-15, 3.6250862475576206e-09])
+
+
+def test_junction_j0init(junction):
+
+    with pytest.raises(ValueError, match=r"J0ref and n must be same size"):
+        junction._J0init(1e-15)
+
+    junction._J0init([1e-15, 1e-9])
+
+    np.testing.assert_allclose(junction.J0, [1e-15, 1e-9])
+    np.testing.assert_allclose(junction.J0ratio, [7.609626, 2.75855506])
+
+
+def test_jem(junction):
+
+    np.testing.assert_almost_equal(junction.Jem(0.6), 1.8227873411146403e-06)
+    np.testing.assert_almost_equal(junction.Jem(-0.6), 0.0)
+
+
+def test_notdiode(junction):
+
+    assert not junction.notdiode()
+    junction.set(J0ratio=[0, 0])
+    assert junction.notdiode()
+
+
+def test_Jmultidiodes(junction):
+
+    np.testing.assert_almost_equal(junction.Jmultidiodes(0.56), 0.00019985765193700707)
+
+
+def test_JshuntRBB(junction):
+
+    junction.set(RBB=None, Gsh=1e-4)
+    np.testing.assert_almost_equal(junction.JshuntRBB(2), 0.0002)
+    junction.set(RBB="JFG")
+    np.testing.assert_almost_equal(junction.JshuntRBB(-3), -3.033350517003164)
+    junction.set(RBB="bishop")
+    np.testing.assert_almost_equal(junction.JshuntRBB(-18), -0.0018153659543416658)
+
+
+def test_Vdiode(junction):
+
+    np.testing.assert_almost_equal(junction.Vdiode(0), 0.7849550554937345)
+    np.testing.assert_almost_equal(junction.Vdiode(-40e-3), 0, decimal=5)
+    np.testing.assert_equal(junction.Vdiode(-41e-3), np.nan)
+
+    junction.set(RBB="bishop", Gsh=1e-4)
+    np.testing.assert_almost_equal(junction.Vdiode(-41e-3), -9.652384228088378)
+    junction.set(RBB="JFG")
+    np.testing.assert_almost_equal(junction.Vdiode(-41e-3), -0.9224606102628319)
+
+    junction.set(J0ratio=[0, 0])
+    np.testing.assert_almost_equal(junction.Vdiode(-41e-3), 0)
+
+
+def test_Vmid(junction):
+
+    np.testing.assert_almost_equal(junction.Vmid(0), 0)
+    np.testing.assert_almost_equal(junction.Vmid(0.5), 0.5)
+
+    junction.set(Rser=0.73)
+    np.testing.assert_almost_equal(junction.Vmid(0), 0.029200002624152344)
+    np.testing.assert_almost_equal(junction.Vmid(0.5), 0.5291205858003947)
